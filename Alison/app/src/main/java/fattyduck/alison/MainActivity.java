1521a224d93ec53173482ee86b6b61e7e28e8be9
unknown <dison.ruan@yahoo.com>
2015-05-01 02:13:02 -0400

    Training at home

---
+++ Alison/app/src/main/java/fattyduck/alison/MainActivity.java
@@ -0,0 +1,64 @@
+package fattyduck.alison;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.graphics.Color;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //layout
+        RelativeLayout duckLayout = new RelativeLayout(this);
+        duckLayout.setBackgroundColor(Color.GREEN);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        //button
+        Button redButton = new Button(this);
+        redButton.setText("Click me for fun");
+        redButton.setBackgroundColor(Color.RED);
+
+
+        duckLayout.addView(redButton, buttonDetails);
+        setContentView(duckLayout);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
